5f9a2f78e00fc302f3163ef1f4e266d9c8d6c726
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2021-02-08 22:09:13 +0530

    Add files via upload

--- Reflection04.java
+++ Reflection04.java
@@ -8,11 +8,12 @@ public class Reflection04 {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		mainClass mc=new mainClass();
-		Field [] fields1= mc.getClass().getFields();
+		mainClass mc=new mainClass();//declaring the mainclass
+		Field [] fields1= mc.getClass().getFields();//getting the public fields in the main class
 		System.out.printf("There are %d fields\n", fields1.length);
 		for(Field f: fields1)
 		{
+			//displaying the identified data in the field
 			System.out.printf(" Field name=%s \nType=%s \nValue=%s \n", f.getName() , f.getType() , f.get(mc));
 		}
 	}
