c435bb2db083661ffb1879a91c96a95e0694bd1b
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2021-02-08 21:19:43 +0530

    Add files via upload
    Here the mainclass is tested against the reflection API 3

---
+++ Reflection03.java
@@ -0,0 +1,14 @@
+package task4;
+
+
+
+public class Reflection03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mainClass mc=new mainClass();
+		System.out.println("class ="+mc.getClass());
+		System.out.println("class name="+mc.getClass().getName());
+	}
+
+}
