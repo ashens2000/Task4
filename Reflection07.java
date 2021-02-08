ea37c04d12481288c2541a8e60a57c65dd191997
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2021-02-08 23:21:30 +0530

    Add files via upload
    Here the declaredField method is tested again by setting the accessible property true. Since the accessibility is set to true , it allows the method to access the private fields

---
+++ Reflection07.java
@@ -0,0 +1,23 @@
+package task4;
+
+import java.lang.reflect.Field;
+
+
+
+public class Reflection07 {
+
+	public static void main(String[] args)  throws Exception{
+		mainClass mc=new mainClass();
+		Field [] fields= mc.getClass().getDeclaredFields();
+		System.out.printf("There are %d fields\n", fields.length);
+		for(Field f: fields)
+		{
+			f.setAccessible(true);
+			System.out.printf(" field name=%s type=%s  value=%s\n", f.getName() , f.getType() , f.get(mc));
+		}
+
+	}
+
+	}
+
+
