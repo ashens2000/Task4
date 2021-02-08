9ab144c7ff36984c771e931fcf7590d93fcc541f
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2021-02-08 21:16:05 +0530

    Add files via upload
    Here the mainClass is tested using the Reflection API2

---
+++ Reflection02.java
@@ -0,0 +1,17 @@
+package task4;
+
+public class Reflection02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		mainClass mc=new mainClass();
+		//mc.petnameforfname();
+		
+		mc.petnameforlname();
+		System.out.println("\n mc= "+mc);
+		
+
+	}
+
+}
