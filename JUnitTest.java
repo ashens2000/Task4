aafd8619c280360e446ffdeb53e030de8fcf89e2
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2021-02-09 20:56:50 +0530

    Add files via upload
    Updated JUnitTest class

---
+++ JUnitTest.java
@@ -0,0 +1,14 @@
+package task4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
